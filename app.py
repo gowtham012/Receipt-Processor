57474cbe43ce03d1964cdfea5deb0ed192994177
gowtham kumar <gouthamsolleti3@gmail.com>
2024-07-24 21:06:32 -0400

    final_commit

--- app.py
+++ app.py
@@ -70,4 +70,5 @@ def get_points(receipt_id):
     return jsonify({"points": points}), 200
     
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8009)
+    app.run(debug=True, host='0.0.0.0', port=8000)
+
